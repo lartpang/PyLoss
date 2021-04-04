7e404091371a8222caa255677826310037a3b2f6
lartpang <lartpang@163.com>
2021-04-04 17:26:32 +0800

    update the config about the publishing package

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pyloss",
+    packages=find_packages(),
+    version="0.1.0",
+    license="MIT",
+    description="Some loss functions for deep learning.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="lartpang",
+    author_email="lartpang@gmail.com",
+    url="https://github.com/lartpang/PyLoss",
+    keywords=[
+        "image segmentation",
+        "loss",
+        "deep learning",
+    ],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
